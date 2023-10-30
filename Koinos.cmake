20727e8bdefd2de82d0ba7ab6f694ce4480e0591
Steve Gerbino <steve@gerbino.co>
2023-10-30 14:49:30 -0400

    Setup static analysis checks

--- Koinos.cmake
+++ Koinos.cmake
@@ -17,8 +17,14 @@ option(STATIC_ANALYSIS "Run static analysis during build" OFF)
 if (STATIC_ANALYSIS)
   find_program(CLANG_TIDY clang-tidy)
   if (CLANG_TIDY)
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY};--warnings-as-errors=*;--checks=*)
-    set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY};--warnings-as-errors=*;--checks=*)
+  set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY};
+    --warnings-as-errors=*;
+    --checks=portability-*,clang-analyzer-*,clang-analyzer-cplusplus*;
+    --header-filter=.*)
+  set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY};
+    --warnings-as-errors=*;
+    --checks=portability-*,clang-analyzer-*;
+    --header-filter=.*)
   elseif()
     message(FATAL_ERROR "Static analysis was requested but clang-tidy was not found")
   endif()
