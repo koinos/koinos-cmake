b6b10a2a61d1601f5a5d3ab2c8768bb424a3f80d
Steve Gerbino <steve@gerbino.co>
2023-11-01 17:24:29 -0400

    Move template files into a separate directory and cleanup git module

--- KoinosGit.cmake
+++ KoinosGit.cmake
@@ -1,14 +1,14 @@
-set(CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR})
-if (NOT DEFINED pre_configure_dir)
-  set(pre_configure_dir ${CMAKE_CURRENT_LIST_DIR})
+set(CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR}/Templates)
+if (NOT DEFINED GIT_PRE_CONFIGURE_DIR)
+  set(GIT_PRE_CONFIGURE_DIR ${CMAKE_CURRENT_LIST_DIR}/Templates)
 endif ()
 
-if (NOT DEFINED post_configure_dir)
-  set(post_configure_dir ${CMAKE_BINARY_DIR}/generated)
+if (NOT DEFINED GIT_POST_CONFIGURE_DIR)
+  set(GIT_POST_CONFIGURE_DIR ${CMAKE_BINARY_DIR}/generated)
 endif ()
 
-set(pre_configure_file ${pre_configure_dir}/git_version.cpp.in)
-set(post_configure_file ${post_configure_dir}/git_version.cpp)
+set(GIT_PRE_CONFIGURE_FILE ${GIT_PRE_CONFIGURE_DIR}/git_version.cpp.in)
+set(GIT_POST_CONFIGURE_FILE ${GIT_POST_CONFIGURE_DIR}/git_version.cpp)
 
 function(koinos_git_write git_hash)
   file(WRITE ${CMAKE_BINARY_DIR}/git-state.txt ${git_hash})
@@ -33,12 +33,12 @@ function(koinos_check_git_version)
   )
 
   koinos_git_read(GIT_HASH_CACHE)
-  if (NOT EXISTS ${post_configure_dir})
-    file(MAKE_DIRECTORY ${post_configure_dir})
+  if (NOT EXISTS ${GIT_POST_CONFIGURE_DIR})
+    file(MAKE_DIRECTORY ${GIT_POST_CONFIGURE_DIR})
   endif ()
 
-  if (NOT EXISTS ${post_configure_dir}/git_version.h)
-    file(COPY ${pre_configure_dir}/git_version.h DESTINATION ${post_configure_dir})
+  if (NOT EXISTS ${GIT_POST_CONFIGURE_DIR}/git_version.h)
+    file(COPY ${GIT_PRE_CONFIGURE_DIR}/git_version.h DESTINATION ${GIT_POST_CONFIGURE_DIR})
   endif()
 
   if (NOT DEFINED GIT_HASH_CACHE)
@@ -47,12 +47,12 @@ function(koinos_check_git_version)
 
   # Only update the git_version.cpp if the hash has changed. This will
   # prevent us from rebuilding the project more than we need to.
-  if (NOT ${GIT_HASH} STREQUAL ${GIT_HASH_CACHE} OR NOT EXISTS ${post_configure_file})
+  if (NOT ${GIT_HASH} STREQUAL ${GIT_HASH_CACHE} OR NOT EXISTS ${GIT_POST_CONFIGURE_FILE})
     # Set che GIT_HASH_CACHE variable the next build won't have
     # to regenerate the source file.
     koinos_git_write(${GIT_HASH})
 
-    configure_file(${pre_configure_file} ${post_configure_file} @ONLY)
+    configure_file(${GIT_PRE_CONFIGURE_FILE} ${GIT_POST_CONFIGURE_FILE} @ONLY)
   endif ()
 
 endfunction()
@@ -60,11 +60,11 @@ endfunction()
 function(koinos_add_git_target)
   add_custom_target(check_git COMMAND ${CMAKE_COMMAND}
     -DRUN_CHECK_GIT_VERSION=1
-    -Dpre_configure_dir=${pre_configure_dir}
-    -Dpost_configure_file=${post_configure_dir}
+    -DGIT_PRE_CONFIGURE_DIR=${GIT_PRE_CONFIGURE_DIR}
+    -DGIT_POST_CONFIGURE_FILE=${GIT_POST_CONFIGURE_DIR}
     -DGIT_HASH_CACHE=${GIT_HASH_CACHE}
     -P ${CURRENT_LIST_DIR}/KoinosGit.cmake
-    BYPRODUCTS ${post_configure_file}
+    BYPRODUCTS ${GIT_POST_CONFIGURE_FILE}
   )
 
   add_library(git_version ${CMAKE_BINARY_DIR}/generated/git_version.cpp)
