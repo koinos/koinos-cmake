d03e2a3cfd3edd85c69de00e4ac0646cca7d03ee
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-05 13:29:55 -0800

    Use submodules for all Koinos repos

--- Koinos.cmake
+++ Koinos.cmake
@@ -16,7 +16,7 @@ include(HunterGate)
 HunterGate(
   URL "https://github.com/cpp-pm/hunter/archive/v0.25.0.tar.gz"
   SHA1 "a1296b351dbfaf036c92d85c1bdb461f615849fa"
-  LOCAL
+  FILEPATH "${CMAKE_MODULE_PATH}/Hunter/config.cmake"
 )
 
 include(KoinosCompilerOptions)
