3881f0c1d05e38187adab13021739f1c615be63e
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2023-12-06 14:17:52 -0800

    elseif() -> else() in KoinosFormat.cmake

--- KoinosFormat.cmake
+++ KoinosFormat.cmake
@@ -2,16 +2,16 @@ find_program(CLANG_FORMAT clang-format)
 
 if (NOT CLANG_FORMAT)
   message(DEBUG "Configuring project without formatting targets, install 'clang-format' to enable")
-elseif()
+else()
   add_custom_target(format.check)
   add_custom_Target(format.fix)
 endif()
 
 function(koinos_add_format_target NAME)
-  
+
   if (NOT CLANG_FORMAT)
     message(DEBUG "No formatting target created for ${NAME}, install 'clang-format' to enable")
-  elseif()
+  else()
     get_target_property(TARGET_SOURCES ${NAME} SOURCES)
     get_target_property(TARGET_SOURCE_DIR ${NAME} SOURCE_DIR)
     list(TRANSFORM TARGET_SOURCES PREPEND "${TARGET_SOURCE_DIR}/")
